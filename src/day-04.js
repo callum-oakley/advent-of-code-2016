e96bcec1883a204a151a5f06b7361ef32b152751
Callum Oakley <c.oakley108@gmail.com>
2016-12-04 11:55:02 +0000

    day 04, part 1

---
+++ src/day-04.js
@@ -0,0 +1,38 @@
+// `group([0, 1, 1, 0, 0, 0, 3])` returns `[[0], [1, 1], [0, 0, 0], [3]]`
+function group (arr) {
+  var groups = [], i = 0;
+  while (i < arr.length) {
+    var group = [];
+    do {
+      group.push(arr[i++]);
+    } while (arr[i] === group[0])
+    groups.push(group);
+  }
+  return groups;
+};
+
+// sort groups by length first, and then alphabetically
+function compare(x, y) {
+  if (y.length - x.length) {
+    return y.length - x.length;
+  } else {
+    return x[0] > y[0] ? 1 : -1;
+  }
+}
+
+function checksum(name) {
+  const letters = name.split("").filter(x => x !== "-").sort(),
+    rankedLetters = group(letters).sort(compare).map(x => x[0]);
+  return rankedLetters.slice(0, 5).reduce((x, y) => x + y, "");
+}
+
+function isReal(room) {
+  return checksum(room.name) === room.checksum;
+}
+
+export function part1(input) {
+  return input.filter(isReal).map(x => x.id).reduce((x, y) => x + y);
+}
+
+export function part2(input) {
+}
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import * as day02 from "./day-02.js";
 import input02 from "./input-02.json";
 import * as day03 from "./day-03.js";
 import input03 from "./input-03.json";
+import * as day04 from "./day-04.js";
+import input04 from "./input-04.json";
 
 console.log(`day 01 - part 1: ${day01.part1(input01)}`);
 console.log(`       - part 2: ${day01.part2(input01)}`);
@@ -11,3 +13,5 @@ console.log(`day 02 - part 1: ${day02.part1(input02)}`);
 console.log(`       - part 2: ${day02.part2(input02)}`);
 console.log(`day 03 - part 1: ${day03.part1(input03)}`);
 console.log(`       - part 2: ${day03.part2(input03)}`);
+console.log(`day 04 - part 1: ${day04.part1(input04)}`);
+console.log(`       - part 2: ${day04.part2(input04)}`);
